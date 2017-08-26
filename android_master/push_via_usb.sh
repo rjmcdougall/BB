d8313a53edfc4d03fbbb4d5d8107ec7b04037e3d
Richard McDougall <git@3emus.com>
2017-08-25 18:21:55 -0700

    fix

--- android_master/push_via_usb.sh
+++ android_master/push_via_usb.sh
@@ -5,6 +5,7 @@ echo "Hope you remembered to set the name and type in boot.prop"
 
 ~/Library/Android/sdk/platform-tools/adb install -r -g bb.apk
 ~/Library/Android/sdk/platform-tools/adb root
+sleep 10
 ~/Library/Android/sdk/platform-tools/adb shell setprop persist.adb.tcp.port 5555
 ~/Library/Android/sdk/platform-tools/adb remount
 sleep 10
