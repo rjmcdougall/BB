2a34a3a6914c877257d3cf61b150f4a84460be8e
danielkeithw <danielkeithw@gmail.com>
2017-12-02 12:01:43 -0500

    fix uploader

--- burnerboard.com/routes/media/index.js
+++ burnerboard.com/routes/media/index.js
@@ -1,14 +1,18 @@
-
 var express = require('express');
 var router = express.Router();
 var util = require('util')
+ 
+var format = require('util').format;
+var Multer = require('multer');
+var bodyParser = require('body-parser');
+var process = require('process'); // Required to mock environment variables
 
 // Imports the Google Cloud client library
 const Storage = require('@google-cloud/storage');
+const storage = Storage(); 
+const bucket = storage.bucket('burner-board');
 
-// Creates a client
-const storage = new Storage();
-
+router.use(bodyParser.json());
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/drive-nodejs-quickstart.json
@@ -17,16 +21,21 @@ var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'media.json';
 
+const MUSIC_PATH = "BurnerBoardMusic";
+
+// Multer is required to process file uploads and make them available via
+// req.files.
+const upload = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 20 * 1024 * 1024 // no larger than 5mb, you can change as needed.
+  }
+});
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   listFiles(res);
 });
-  
-module.exports = router;
-
-const options = {
-  prefix: '',
-};
 
 function listFiles(res) {
   var callback = function(err, files, nextQuery, apiResponse) {
@@ -39,12 +48,57 @@ function listFiles(res) {
   };
 
   // Lists files in the bucket, filtered by a prefix
-  var stuff = storage
-  .bucket('burner-board')
+  var stuff = bucket
   .getFiles({
     autoPaginate: false,
     delimiter: '/',
-    prefix: 'BurnerBoardMusic/'
+    prefix: MUSIC_PATH + '/'
   }, callback);
 
 }
+
+// [START form]
+// Display a form for uploading files.
+router.get('/upload', (req, res) => {
+    res.render('uploadForm', { title: 'burnerboard.com' });
+  });
+
+// [START process]
+// Process the file upload and upload to Google Cloud Storage.
+router.post('/upload', upload.single('file'), (req, res, next) => {
+  if (!req.file) {
+    res.status(400).send('No file uploaded.');
+    return;
+  }
+
+  // Create a new blob in the bucket and upload the file data.
+  var contenttype = '';
+  if(req.file.originalname.endsWith('mp3'))
+    contenttype = 'audio/mpeg';
+  else if(req.file.originalname.endsWith('mp4'))
+    contenttype = 'video/mp4';
+
+  var filepath = MUSIC_PATH + '/' + req.file.originalname;
+  const file = bucket.file(filepath);
+
+  const fileStream = file.createWriteStream({
+    metadata:{
+      contentType: contenttype
+    }
+  });
+
+  fileStream.on('error', (err) => {
+    next(err);
+  });
+
+  fileStream.on('finish', () => {
+    // The public URL can be used to directly access the file via HTTP.
+    const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${file.name}`);
+    res.status(200).send(publicUrl);
+  });
+
+  fileStream.end(req.file.buffer);
+});
+// [END process]
+
+module.exports = router;
