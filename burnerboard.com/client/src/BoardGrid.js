e89cb32ccbbab648897c02718cc4df2971721482
danielkeithw <danielkeithw@gmail.com>
2017-12-28 22:09:18 -0500

    data bind board grid

--- burnerboard.com/client/src/BoardGrid.js
+++ burnerboard.com/client/src/BoardGrid.js
@@ -1,5 +1,3 @@
-
-//const exampleWrapper = require('../components/exampleWrapper');
 import React, { Component } from 'react';
 import ReactDataGrid from 'react-data-grid';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -7,94 +5,97 @@ import PropTypes from 'prop-types';
 
 // Custom Formatter component
 class PercentCompleteFormatter extends React.Component {
-  static propTypes = {
-    value: PropTypes.number.isRequired
-  };
+    static propTypes = {
+        value: PropTypes.number.isRequired
+    };
 
-  render() {
-    const percentComplete = this.props.value + '%';
-    return (
-      <div className="progress" style={{marginTop: '20px'}}>
-        <div className="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{width: percentComplete}}>
-          {percentComplete}
-        </div>
-      </div>);
-  }
+    render() {
+        const percentComplete = this.props.value + '%';
+        return (
+            <div className="progress" style={{ marginTop: '20px' }}>
+                <div className="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{ width: percentComplete }}>
+                    {percentComplete}
+                </div>
+            </div>);
+    }
 }
 
+const boardsJSON = {
+    boards: [
+        {
+            board_name: "loading boards...",
+            last_seen: "",
+            is_online: false,
+            battery_level: 0
+        }
+    ]
+};
+
+const API = '/boards';
+
 class BoardGrid extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.createRows();
-    this._columns = [
-      {
-        key: 'id',
-        name: 'ID',
-        width: 80
-      },
-      {
-        key: 'task',
-        name: 'Title'
-      },
-      {
-        key: 'priority',
-        name: 'Priority'
-      },
-      {
-        key: 'issueType',
-        name: 'Issue Type'
-      },
-      {
-        key: 'complete',
-        name: '% Complete',
-        formatter: PercentCompleteFormatter
-      },
-      {
-        key: 'startDate',
-        name: 'Start Date'
-      },
-      {
-        key: 'completeDate',
-        name: 'Expected Complete'
-      }
-    ];
+    constructor(props, context) {
+        super(props, context);
 
-    this.state = null;
-  }
+        this._columns = [
+            {
+                key: 'board_name',
+                name: 'Board Name'
+            },
+            {
+                key: 'last_seen',
+                name: 'Last Seen'
+            },
+            {
+                key: 'is_online',
+                name: 'Is Online'
+            },
+            {
+                key: 'battery_level',
+                name: 'Battery Level',
+                formatter: PercentCompleteFormatter
+            }
+        ];
 
-  getRandomDate = (start, end) => {
-    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
-  };
+        this.state = {
+            boardData: boardsJSON.boards,
+        };
 
-  createRows = () => {
-    let rows = [];
-    for (let i = 1; i < 100; i++) {
-      rows.push({
-        id: i,
-        task: 'Task ' + i,
-        complete: Math.min(100, Math.round(Math.random() * 110)),
-        priority: ['Critical', 'High', 'Medium', 'Low'][Math.floor((Math.random() * 3) + 1)],
-        issueType: ['Bug', 'Improvement', 'Epic', 'Story'][Math.floor((Math.random() * 3) + 1)],
-        startDate: this.getRandomDate(new Date(2015, 3, 1), new Date()),
-        completeDate: this.getRandomDate(new Date(), new Date(2016, 0, 1))
-      });
     }
 
-    this._rows = rows;
-  };
+    componentDidMount() {
 
-  rowGetter = (i) => {
-    return this._rows[i];
-  };
+        fetch(API)
+            .then(response => response.json())
+            .then(data => this.setState({
+                boardData: data.map(item => ({
+                    board_name: `${item.board_name}`,
+                    last_seen: `${item.last_seen}`,
+                    is_online: `${item.is_online}`,
+                    battery_level: `${item.battery_level}`,
+                }))
+            }))
+            .catch(error => this.setState({ error }));
 
-  render() {
-    return  (
-      <ReactDataGrid
-        columns={this._columns}
-        rowGetter={this.rowGetter}
-        rowsCount={this._rows.length}
-        minHeight={500} />);
-  }
+    }
+
+    getRandomDate = (start, end) => {
+        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
+    };
+
+
+    rowGetter = (i) => {
+        return this.state.boardData[i];
+    };
+
+    render() {
+        return (
+            <ReactDataGrid
+                columns={this._columns}
+                rowGetter={this.rowGetter}
+                rowsCount={this.state.boardData.length}
+                minHeight={500} />);
+    }
 }
 
 export default BoardGrid;
