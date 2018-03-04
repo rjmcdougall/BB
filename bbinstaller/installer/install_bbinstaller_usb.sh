975f32fe8629551bcfd39c109defc76b2bcc3602
Richard McDougall <git@3emus.com>
2018-03-03 18:03:44 -0800

    New installer pointing to google.com

--- bbinstaller/installer/install_bbinstaller_usb.sh
+++ bbinstaller/installer/install_bbinstaller_usb.sh
@@ -5,7 +5,7 @@
 ~/Library/Android/sdk/platform-tools/adb shell rm -r /system/priv-app/com.richardmcdougall.bb*
 ~/Library/Android/sdk/platform-tools/adb reboot
 sleep 40
-~/Library/Android/sdk/platform-tools/adb install -rg bbinstaller-release.apk
+~/Library/Android/sdk/platform-tools/adb install -rg release/bbinstaller-release.apk
 ~/Library/Android/sdk/platform-tools/adb root
 ~/Library/Android/sdk/platform-tools/adb remount
 ~/Library/Android/sdk/platform-tools/adb shell cp -rp  /data/app/com.richardmcdougall.bbinstaller* /system/priv-app
