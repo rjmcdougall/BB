74bc5b11b63288bf60499c0929990edaff19b7fd
danielkeithw <danielkeithw@gmail.com>
2018-03-11 13:55:12 -0400

    initial navigation for bbmobile.

--- BBMobile/App.js
+++ BBMobile/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import { StackNavigator } from 'react-navigation'; // Version can be specified in package.json
+import {Button} from 'react-native';
+import HomeScreen from './HomeScreen';
+import BBComView from './BBComView';
+import ManageBoardSettings from './ManageBoardSettings';
+  
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    BBCom: { 
+      screen: BBComView,
+    },   
+    BoardSettings: { 
+      screen: ManageBoardSettings,
+    }, 
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello BurnerBoard App!</Text>
-      </View>
-    );
+    return <RootStack />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
