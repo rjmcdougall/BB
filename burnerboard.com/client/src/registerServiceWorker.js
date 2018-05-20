d08aae092b58c3bd81f78b6d7c5082cec4d80537
danielkeithw <danielkeithw@gmail.com>
2018-05-20 19:56:11 -0400

    Turn off caching index.html and registerServiceWorker

--- burnerboard.com/client/src/registerServiceWorker.js
+++ burnerboard.com/client/src/registerServiceWorker.js
@@ -99,6 +99,7 @@ function checkValidServiceWorker(swUrl) {
 		});
 }
 
+
 export function unregister() {
 	if ("serviceWorker" in navigator) {
 		navigator.serviceWorker.ready.then(registration => {
