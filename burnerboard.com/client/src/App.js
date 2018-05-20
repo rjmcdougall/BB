ab8a41a07269bb10ba647eccfabb34cecf0dce8d
danielkeithw <danielkeithw@gmail.com>
2018-05-20 10:35:19 -0400

    cleanup

--- burnerboard.com/client/src/App.js
+++ burnerboard.com/client/src/App.js
@@ -28,75 +28,71 @@ class App extends Component {
 	}
 
 	// @ts-ignore
-	responseGoogle(response) {
+	async responseGoogle(response) {
 		console.log("google response: " + JSON.stringify(response));
 
 		if (response.error != null && response.error !== "") {
 
 			var errorText = "";
-			if(response.error.contains("http://burnerboard.com"))
+			if (response.error.contains("http://burnerboard.com"))
 				errorText = "YOU MUST USE HTTPS!!!!!!!!!!!!! GET OFF OF HTTP ITS NOT SECURE!!!!!!";
 			else
-				errorText = "Error: " + response.error + " " + response.details + " Please try again." ;
+				errorText = "Error: " + response.error + " " + response.details + " Please try again.";
 
-
-			this.setState({ buttonText: errorText});
+			this.setState({ buttonText: errorText });
 		}
 		else {
 
 			var id_token = response.getAuthResponse().id_token;
-			var access_token  = response.getAuthResponse().access_token;
+			var access_token = response.getAuthResponse().access_token;
 			console.log("id token : " + id_token);
 
 			const app = this;
-
 			var API = "/users/Auth";
 
-			fetch(API, {
-				method: "POST",
-				headers: {
-					"Accept": "application/json",
-					"Content-Type": "application/json",
-					"authorization": id_token,
-				}
-			})
-				.then(res => {
-					if (!res.ok) {
-						res.text().then(function (text) {
-
-							app.setState({
-								JWT: "",
-								buttonText: text,
-							});
-						});
+			try {
+				var res = await fetch(API, {
+					method: "POST",
+					headers: {
+						"Accept": "application/json",
+						"Content-Type": "application/json",
+						"authorization": id_token,
 					}
-					else {
+				});
 
-						window.sessionStorage.setItem("JWT", id_token);
-						window.sessionStorage.setItem("accessToken", access_token);
-						console.log("JWT stored in sessionStorage: " + id_token);
-						console.log("accessToken stored in sessionStorage: " + access_token);
+				if (!res.ok) {
+					res.text().then(function (text) {
 
-						app.setState({
-							JWT: window.sessionStorage.JWT,
-							buttonText: "",
-						});
-					}
-				})
-				.catch((err) => {
-
-					err.text().then(function (text) {
 						app.setState({
 							JWT: "",
 							buttonText: text,
 						});
 					});
+				}
+				else {
 
-				});
+					window.sessionStorage.setItem("JWT", id_token);
+					window.sessionStorage.setItem("accessToken", access_token);
+					console.log("JWT stored in sessionStorage: " + id_token);
+					console.log("accessToken stored in sessionStorage: " + access_token);
 
+					app.setState({
+						JWT: window.sessionStorage.JWT,
+						buttonText: "",
+					});
+				}
+			}
+			catch (err) {
+				try {
+					var text = await err.text();
+					app.setState({
+						JWT: "",
+						buttonText: text,
+					});
+				}
+				finally { }
+			}
 		}
-
-
 	}
 
 	render() {
@@ -105,12 +101,11 @@ class App extends Component {
 		var appBody = "";
 		var queryJWT = this.getQueryVariable("JWT");
 
-		if(queryJWT && queryJWT!=""){
+		if (queryJWT && queryJWT != "") {
 			console.log(queryJWT);
 			window.sessionStorage.setItem("JWT", queryJWT);
 		}
-		
- 
+
 		if (window.sessionStorage.JWT != null)
 			JWT = window.sessionStorage.JWT;
 
