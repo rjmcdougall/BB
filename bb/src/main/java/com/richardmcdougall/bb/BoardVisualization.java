d20781c4a350b6bc21c0a1dac95c697405fb9676
Richard McDougall <git@3emus.com>
2018-05-28 14:02:12 -0700

    location map

--- bb/src/main/java/com/richardmcdougall/bb/BoardVisualization.java
+++ bb/src/main/java/com/richardmcdougall/bb/BoardVisualization.java
@@ -75,6 +75,9 @@ public class BoardVisualization {
         mBurnerBoard = board;
         mBBservice = service;
         l("Starting Board Visualization " + mBurnerBoard.boardType + " on " + mBurnerBoard.boardId);
+
+
+        // TODO: Replace this with calls into the BurnerBoard class to discover
         if (mBurnerBoard.boardType.contains(new String("Classic"))) {
             l("Starting Classic Visualization");
             mBoardWidth = 10;
@@ -236,6 +239,11 @@ public class BoardVisualization {
             case "modeAudioTile()":
                 frameRate = 12;
                 modeAudioTile();
+                break;
+
+            case "modeMeteor()":
+                frameRate = 12;
+                modeMeteor();
                 break;
 
         }
@@ -1945,5 +1953,48 @@ public class BoardVisualization {
         }
     }
 
+    // Woodson's pool meteor
+    private final static int kMeteorLeds = 55;
+    private final static int kMeteorIncrementor = 4;
+    private final static int kMeteorSize = 10;
+    private final static int kFadeAmount = 25;
+    private final static int kShiftLimit = 255; // Must be less than kRedStart
+    private final static int kRedStart = 255;
+    private final static int kGreenStart = 215;
+    private final static int kBlueStart = 0;
 
+    private int meteorColorIter = 0;
+    private int meteorPixelIter = 0;
+
+    void modeMeteor() {
+
+        int meteorRedShift = kRedStart - meteorColorIter;
+        int meteorBlueShift = kBlueStart;
+        int meteorGreenShift = kGreenStart;
+
+        // fade brightness all LEDs one step
+        mBurnerBoard.fadePixels(kFadeAmount);
+
+        // draw meteor
+        for(int j = 0; j < kMeteorSize; j++) {
+            if(((meteorPixelIter - j) < kMeteorLeds) && ((meteorPixelIter - j) >= 0)) {
+                mBurnerBoard.setPixel(16, meteorPixelIter-j,
+                        meteorRedShift, meteorGreenShift, meteorBlueShift);
+            }
+        }
+
+        meteorPixelIter += kMeteorIncrementor;
+        if (meteorPixelIter > (kMeteorLeds * 2)) {
+            meteorPixelIter = 0;
+            meteorColorIter++;
+            meteorColorIter++;
+            if (meteorColorIter > kShiftLimit) {
+                meteorColorIter = 0;
+            }
+        }
+
+        mBurnerBoard.flush();
+
+        return;
+    }
 }
