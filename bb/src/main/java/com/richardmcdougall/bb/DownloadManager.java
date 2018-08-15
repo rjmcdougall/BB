f948687f9df6fb80f020fc02cf27f3c2f0b88b8b
Jos Boumans <jos@dwim.org>
2018-08-15 11:08:48 -0700

    Add diagnostics to DL manager to see what is failing. Helps diagnose bug #169 (but does not fix it)

--- bb/src/main/java/com/richardmcdougall/bb/DownloadManager.java
+++ bb/src/main/java/com/richardmcdougall/bb/DownloadManager.java
@@ -442,6 +442,8 @@ public class DownloadManager {
                 String[] dTypes = new String[]{"audio", "video"};
                 JSONArray changedFiles = new JSONArray();
 
+                Log.d(TAG, "Downloaded JSON: " + dirTxt);
+
                 // determine changes
                 for (int i = 0; i < dTypes.length; i++) {
                     JSONArray tList = dir.getJSONArray(dTypes[i]);
@@ -476,8 +478,11 @@ public class DownloadManager {
                     mDM.dataDirectory = dir;
                     CleanupOldFiles();
                     new File(mDM.mFilesDir, "directory.json.tmp").renameTo(new File(mDM.mFilesDir, "directory.json"));
-                    if (mDM.onProgressCallback != null)
-                        mDM.onProgressCallback.onVoiceCue("Finished downloading " + String.valueOf(changedFiles.length()) + " files. Media ready.");
+                    if (mDM.onProgressCallback != null) {
+                        String diag = "Finished downloading " + String.valueOf(changedFiles.length()) + " files. Media ready.";
+                        Log.d(TAG, diag);
+                        mDM.onProgressCallback.onVoiceCue(diag);
+                    }
                 }
                 return true;
 
