be6fc9b9b9dd18a01aa21f7cb7b252fa341f9954
Dan Wilson <danielkeithw@gmail.com>
2019-01-01 20:58:56 -0500

    upgrade google-auth-library to 2.0.2

--- burnerboard.com/routes/UserStore.js
+++ burnerboard.com/routes/UserStore.js
@@ -1,46 +1,36 @@
 const constants = require("./Constants");
-const GoogleAuth = require("google-auth-library");
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(constants.CLIENT_ID);
 const Datastore = require("@google-cloud/datastore");
 const datastore = new Datastore({
 	projectId: constants.PROJECT_ID,
 });
 
 exports.verifyJWT = async function (JWT) {
-	return new Promise((resolve, reject) => {
 
-		var auth = new GoogleAuth;
-		var client = new auth.OAuth2(constants.CLIENT_ID, "", "");
-		client.verifyIdToken(
-			JWT,
-			constants.CLIENT_ID,
-			function (e, login) {
-				if (e) {
-					return reject(e);
-				}
-				else {
-					var payload = login.getPayload();
-					var email = payload.email;
-					var emailFound = null;
+	try {
+		const ticket = await client.verifyIdToken({
+			idToken: JWT,
+			audience: constants.CLIENT_ID
+		});
+		const payload = ticket.getPayload();
+		var email = payload.email;
 
-					const emailExists = datastore.createQuery("user")
-						.filter("email", "=", email);
+		var emailExists = datastore.createQuery("user")
+			.filter("email", "=", email);
 
-					datastore
-						.runQuery(emailExists)
-						.then(results => {
-							emailFound = (results[0].length > 0);
+		var results = await datastore.runQuery(emailExists);
+		var emailFound = (results[0].length > 0);
 
-							if (emailFound) {
-								return resolve(JWT);
-							}
-							else {
-								return reject(new Error("Not Authorized for BurnerBoard.com"));
-							}
-						})
-						.catch(err => {
-							reject(err);
-						});
-				}
-			});
-	});
+		if (emailFound) {
+			return JWT;
+		}
+		else {
+			return new Error("Not Authorized for BurnerBoard.com");
+		}
+	}
+	catch (error) {
+		return error;
+	}
+
 };
