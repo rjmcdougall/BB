a1dd470af2a478823e84fe659a9046cf06f09c08
Dan Wilson <danielkeithw@gmail.com>
2019-05-05 14:04:33 -0700

    clean up BLE logging

--- BBMobile/BLEBoardData.js
+++ BBMobile/BLEBoardData.js
@@ -9,14 +9,12 @@ var lock = new AsyncLock();
 
 exports.refreshMediaState = async function (mediaState) {
 
-	mediaState = BLEIDs.BLELogger(mediaState, "BLE: refreshMediaState Getting state ", false);
 	if (mediaState.connectedPeripheral) {
 		try {
-			mediaState = BLEIDs.BLELogger(mediaState, "BLE: Getting state ", false);
+			mediaState = BLEIDs.BLELogger(mediaState, "BLE: requesting state ", false);
 			if (await sendCommand(mediaState, "getall", "") == false) {
 				return mediaState;
 			}
-			mediaState = BLEIDs.BLELogger(mediaState, "BLE: RefreshMediaState Complete: ", false);
 			return mediaState;
 		}
 		catch (error) {
@@ -35,30 +33,37 @@ exports.updateMediaState = function (mediaState, newMedia) {
 	console.log("BLE: new update from brain");
 	if (newMedia.boards) {
 		console.log("BLE: updated boards");
+		mediaState = BLEIDs.BLELogger(mediaState, "BLE: updated boards", false);
 		mediaState.boards = newMedia.boards;
 	}
 	if (newMedia.video) {
 		console.log("BLE: updated video: " + JSON.stringify(newMedia.video));
+		mediaState = BLEIDs.BLELogger(mediaState, "BLE: updated video", false);
 		mediaState.video = newMedia.video;
 	}
 	if (newMedia.audio) {
 		console.log("BLE: updated audio: " + JSON.stringify(newMedia.audio));
+		mediaState = BLEIDs.BLELogger(mediaState, "BLE: updated audio", false);
 		mediaState.audio = newMedia.audio;
 	}
 	if (newMedia.state) {
+		mediaState = BLEIDs.BLELogger(mediaState, "BLE: updated state", false);
 		console.log("BLE: updated state: " + JSON.stringify(newMedia.state));
 		mediaState.state = newMedia.state;
 	}
 	if (newMedia.btdevices) {
+		mediaState = BLEIDs.BLELogger(mediaState, "BLE: updated devices", false);
 		console.log("BLE: updated btdevices: " + JSON.stringify(newMedia.btdevices));
 		if(newMedia.btdevices.length>0)
 			mediaState.devices = newMedia.btdevices;
 	}
 	if (newMedia.locations) {
+		mediaState = BLEIDs.BLELogger(mediaState, "BLE: updated locations", false);
 		console.log("BLE: updated locations: " + JSON.stringify(newMedia.locations));
 		mediaState.locations = newMedia.locations;
 	}
 	if (newMedia.battery) {
+		mediaState = BLEIDs.BLELogger(mediaState, "BLE: updated battery", false);
 		console.log("BLE: updated battery: " + JSON.stringify(newMedia.battery));
 		mediaState.battery = newMedia.battery;
 	}
@@ -69,7 +74,7 @@ sendCommand = function (mediaState, command, arg) {
 	// Send request command
 	if (mediaState.connectedPeripheral.connected == Constants.CONNECTED) {
 		console.log("BLE: send command " + command + " " + arg + " on device " + mediaState.connectedPeripheral.id);
-		mediaState = BLEIDs.BLELogger(mediaState, "BLE: send command " + command + "on device " + mediaState.connectedPeripheral.id, false);
+		mediaState = BLEIDs.BLELogger(mediaState, "BLE: send command " + command + " on device " + mediaState.connectedPeripheral.name, false);
 		lock.acquire('send', function (done) {
 			// async work
 			try {
@@ -79,7 +84,7 @@ sendCommand = function (mediaState, command, arg) {
 					BLEIDs.txCharacteristic,
 					data,
 					18); // MTU Size
-				mediaState = BLEIDs.BLELogger(mediaState, "BLE: successfully sent " + command, false);
+				mediaState = BLEIDs.BLELogger(mediaState, "BLE: successfully requested " + command, false);
 			}
 			catch (error) {
 				console.log("BLE: send command " + command + " " + arg + " failed on device " + mediaState.connectedPeripheral.id);
@@ -142,10 +147,10 @@ exports.createMediaState = async function (peripheral) {
 		var mediaState = StateBuilder.blankMediaState();
 		mediaState.connectedPeripheral = peripheral;
 
-		mediaState = BLEIDs.BLELogger(mediaState, "StateBuilder: Getting BLE Data for " + peripheral.name, false);
+		mediaState = BLEIDs.BLELogger(mediaState, "BLE: Getting BLE Data for " + peripheral.name, false);
 		mediaState = await this.refreshMediaState(mediaState);
-		mediaState = BLEIDs.BLELogger(mediaState, "StateBuilder: Gettig Boards Data from API ", false);
 
+		mediaState = BLEIDs.BLELogger(mediaState, "API: Gettig Boards Data", false);
 		var boards = await StateBuilder.getBoards();
 		mediaState.boards = boards;
 
