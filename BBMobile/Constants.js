0298c091d781c5f120aed2598353e3e4267fcaaa
Dan Wilson <danielkeithw@gmail.com>
2019-05-30 23:52:13 -0700

    speed up android ble load by 2 seconds

--- BBMobile/Constants.js
+++ BBMobile/Constants.js
@@ -1,6 +1,13 @@
 import { Platform } from "react-native";
 import geoViewport from "@mapbox/geo-viewport";
 
+//UUIDs
+exports.bbUUID = "58fdc6ee-15d1-11e8-b642-0ed5f89f718b";
+exports.UARTservice = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+exports.rxCharacteristic = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+exports.txCharacteristic = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+exports.CCCD = "00002902-0000-1000-8000-00805f9b34fb";
+
 //connection states.
 exports.DISCONNECTED = "Disconnected";
 exports.CONNECTED = "Connected";
@@ -33,21 +40,21 @@ exports.PLAYA_BOUNDS = function() {
 //wait lengths may vary depending on BLE stability and the RN component
 exports.LOCATION_CHECK_INTERVAL = function () {
 	if (Platform.OS == "android")
-		return 15000;
+		return 8000;
 	else
 		return 8000;
 };
 
 exports.CONNECT_SLEEP = function () {
 	if (Platform.OS == "android")
-		return 1000;
+		return 0;
 	else
 		return 0;
 };
 
 exports.RETRIEVE_SERVICES_SLEEP = function () {
 	if (Platform.OS == "android")
-		return 1000;
+		return 0;
 	else
 		return 0;
 };
