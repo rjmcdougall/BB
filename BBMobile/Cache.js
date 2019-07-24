ed66891652314f50b28ed7502bccbd118fcadcb4
Dan Wilson <danielkeithw@gmail.com>
2019-07-23 18:47:41 -0700

    roll back cache to using filesystem and add prefix to prevent mapbox temp from crashing. RN cache was not persistent enough.

--- BBMobile/Cache.js
+++ BBMobile/Cache.js
@@ -1,37 +1,67 @@
-import AsyncStorage from "@react-native-community/async-storage";
- 
+import RNFS from "react-native-fs";
+import Constants from "./Constants";
+
 exports.get = async function (key) {
+	// create a path you want to write to
+
+	var key = Constants.FS_CACHE_HEADER + key;
+
+	var path = RNFS.DocumentDirectoryPath + "/" + key;
 
 	try {
-		var value = JSON.parse(await AsyncStorage.getItem(key));
-		if(value)
-			console.log("Get " + key + " From Cache");
-		else
-			console.log(key + " is not in cache, cannot retrieve.");
+		var dir = await RNFS.readDir(RNFS.DocumentDirectoryPath);
+		var fileExists = dir.filter((item) => {
+			return item.name === key;
+		}).length > 0;
+
+		if (!fileExists) {
+			console.log(key + " Not Found in Cache");
+			return null;
+		}
+
+		console.log("Get " + key + " From Cache");
+		var value = JSON.parse(await RNFS.readFile(path, "utf8"));
 		return value;
 	}
 	catch (error) {
-		console.log(error);
+		console.log("Error: " + error);
 	}
 };
 
 exports.set = async function (key, value) {
- 
+	// create a path you want to write to
+	var key = Constants.FS_CACHE_HEADER + key;
+
+	var path = RNFS.DocumentDirectoryPath + "/" + key;
+
 	try {
-		await AsyncStorage.setItem(key, JSON.stringify(value)); 
+		var dir = await RNFS.readDir(RNFS.DocumentDirectoryPath);
+		var fileExists = dir.filter((item) => {
+			return item.name === key;
+		}).length > 0;
+
+		if(fileExists) {
+			await RNFS.unlink(path);
+			console.log(key + " Found in Cache, Deleting");
+		}
+
 		console.log(key + " set in cache");
+		await RNFS.writeFile(path, JSON.stringify(value), "utf8");
 	}
 	catch (error) {
-		console.log(error);
+		console.log("Error: " + error);
 	}
 };
 
 exports.clear = async function () {
 	try {
-		var dir = await AsyncStorage.getAllKeys();
+		var dir = await RNFS.readDir(RNFS.DocumentDirectoryPath);
 		dir.map(async (item) => {
-			await(AsyncStorage.removeItem(item));
-			console.log(item + " Found in Cache, Deleting");
+			var path = RNFS.DocumentDirectoryPath + "/" + item.name;
+			if(item.name.startsWith(Constants.FS_CACHE_HEADER)){
+				await RNFS.unlink(path);
+				console.log(item.name + " Found in Cache, Deleting");
+			}
 		}) ;
 	}
 	catch (error) {
