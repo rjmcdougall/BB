364b6a0aea8218874edb5f5133d419312400ea3a
Dan Wilson <danielkeithw@gmail.com>
2019-07-23 19:44:31 -0700

    re-add react-native-fs

--- BBMobile/android/app/src/main/java/com/bbmobile/MainApplication.java
+++ BBMobile/android/app/src/main/java/com/bbmobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.bbmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.zyu.ReactNativeWheelPickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+            new RNFSPackage(),
               new AsyncStoragePackage(),
               new RCTMGLPackage(),
               new ReactNativeWheelPickerPackage(),
