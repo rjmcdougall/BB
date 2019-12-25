823bf314df728c0d608db075d47f39e2d296dfaf
Dan Wilson <danielkeithw@gmail.com>
2019-12-25 08:52:00 -0800

    break buttonreceiver out into new class

--- bb/src/main/java/com/richardmcdougall/bb/BBService.java
+++ bb/src/main/java/com/richardmcdougall/bb/BBService.java
@@ -17,7 +17,7 @@ import android.os.SystemClock;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
-import android.view.KeyEvent;
+
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Calendar;
@@ -48,7 +48,6 @@ public class BBService extends Service {
     public Context context;
     public long serverTimeOffset = 0;
     public long serverRTT = 0;
-    public RFClientServer rfClientServer = null;
     /* XXX TODO this string is accessed both directly here in this class, as well as used via getBoardId() on the object it provides. refactor -jib */
     public static String boardId = BurnerBoardUtil.BOARD_ID;
     private int mBoardMode = 1; // Mode of the Ardunio/LEDs
@@ -58,6 +57,7 @@ public class BBService extends Service {
     public MusicPlayer musicPlayer = null;
     public RF radio = null;
     public Gps gps = null;
+    public RFClientServer rfClientServer = null;
     public FindMyFriends findMyFriends = null;
     public BluetoothLEServer bLEServer = null;
     public BluetoothCommands bluetoothCommands = null;
@@ -76,6 +76,7 @@ public class BBService extends Service {
     public BBWifi wifi = null;
     public TextToSpeech voice;
     private BBService.usbReceiver mUsbReceiver = new BBService.usbReceiver();
+    private static BroadcastReceiver buttonReceiver = null;
 
     public BBService() {
     }
@@ -161,6 +162,8 @@ public class BBService extends Service {
                 }
             });
 
+            buttonReceiver = new ButtonReceiver(this);
+
             iotClient = new IoTClient(this);
 
             wifi = new BBWifi(this);
@@ -174,7 +177,7 @@ public class BBService extends Service {
 
             // Register to receive button messages
             IntentFilter filter = new IntentFilter(ACTION.BUTTONS);
-            LocalBroadcastManager.getInstance(this).registerReceiver(mButtonReceiver, filter);
+            LocalBroadcastManager.getInstance(this).registerReceiver(buttonReceiver, filter);
 
             // Register to know when bluetooth remote connects
             context.registerReceiver(btReceive, new IntentFilter(ACTION_ACL_CONNECTED));
@@ -291,63 +294,6 @@ public class BBService extends Service {
         in.putExtra("logMsg", msg);
         LocalBroadcastManager.getInstance(this).sendBroadcast(in);
     }
-
-    // We use this to catch the music buttons
-    private final BroadcastReceiver mButtonReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            final String TAG = "mButtonReceiver";
-
-            //Log.d(TAG, "onReceive entered");
-            String action = intent.getAction();
-
-            if (ACTION.BUTTONS.equals(action)) {
-                Log.d(TAG, "Received BlueTooth key press");
-                buttons actionType = (buttons) intent.getSerializableExtra("buttonType");
-                switch (actionType) {
-                    case BUTTON_KEYCODE:
-                        l("BUTTON_KEYCODE ");
-                        int keyCode = intent.getIntExtra("keyCode", 0);
-                        KeyEvent event = (KeyEvent) intent.getParcelableExtra("keyEvent");
-                        onKeyDown(keyCode, event);
-                        break;
-                    case BUTTON_TRACK:
-                        l("BUTTON_TRACK");
-                        musicPlayer.NextStream();
-                        break;
-                    case BUTTON_MODE_UP:
-                        l("BUTTON_MODE_UP");
-                        setMode(99);
-                        break;
-                    case BUTTON_MODE_DOWN:
-                        l("BUTTON_MODE_DOWN");
-                        setMode(98);
-                        break;
-                    case BUTTON_DRIFT_DOWN:
-                        l("BUTTON_DRIFT_DOWN");
-                        musicPlayer.MusicOffset(-10);
-                        break;
-                    case BUTTON_DRIFT_UP:
-                        l("BUTTON_DRIFT_UP");
-                        musicPlayer.MusicOffset(10);
-                        break;
-                    case BUTTON_VOL_DOWN:
-                        l("BUTTON_VOL_DOWN");
-                        musicPlayer.onVolDown();
-                        break;
-                    case BUTTON_VOL_UP:
-                        l("BUTTON_VOL_UP");
-                        musicPlayer.onVolUp();
-                        break;
-                    case BUTTON_VOL_PAUSE:
-                        l("BUTTON_VOL_PAUSE");
-                        musicPlayer.onVolPause();
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-    };
 
     public void enableMaster(boolean enable) {
         masterRemote = enable;
@@ -473,92 +419,6 @@ public class BBService extends Service {
         return (encoded[0] << 24) + (encoded[1] << 16) + (encoded[2] << 8) + encoded[0];
     }
 
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-        if (!BurnerBoardUtil.kIsRPI) {
-            return onKeyDownBurnerBoard(keyCode, event);
-        } else {
-            return onKeyDownRPI(keyCode, event);
-        }
-    }
-
-    public boolean onKeyDownBurnerBoard(int keyCode, KeyEvent event) {
-        boolean handled = false;
-        if (event.getRepeatCount() == 0) {
-            l("BurnerBoard Keycode:" + keyCode);
-        }
-
-        switch (keyCode) {
-            case 100:
-            case 87: // satachi right button
-                musicPlayer.NextStream();
-                break;
-            case 97:
-            case 20:
-                musicPlayer.MusicOffset(-10);
-                break;
-            case 19:
-                musicPlayer.MusicOffset(10);
-                break;
-            case 24:   // native volume up button
-            case 21:
-                musicPlayer.onVolUp();
-
-                return false;
-            case 25:  // native volume down button
-            case 22:
-                musicPlayer.onVolDown();
-                return false;
-            case 85: // Play button - show battery
-                onBatteryButton();
-                break;
-            case 99:
-                setMode(99);
-                break;
-            case 98:
-                setMode(98);
-                break;
-            case 88: //satachi left button
-                setMode(99);
-                break;
-        }
-        return true;
-    }
-
-    public boolean onKeyDownRPI(int keyCode, KeyEvent event) {
-        boolean handled = false;
-        if (event.getRepeatCount() == 0) {
-            l("RPI Keycode:" + keyCode);
-        }
-
-        switch (keyCode) {
-            case 85: // satachi Play button
-                boardVisualization.NextVideo();
-                break;
-
-            /* Audio stream control */
-            case 87: // satachi right button
-                l("RPI Bluetooth Right Button");
-                musicPlayer.NextStream();
-                break;
-            case 88: //satachi left button
-                l("RPI Bluetooth Left Button");
-                musicPlayer.PreviousStream();
-                break;
-
-            /* Volume control */
-            case 24:   // satachi & native volume up button
-                l("RPI Bluetooth Volume Up Button");
-                musicPlayer.onVolUp();
-                return false;
-            case 25:  // satachi & native volume down button
-                l("RPI Bluetooth Volume Down Button");
-                musicPlayer.onVolDown();
-                return false;
-        }
-        return true;
-    }
-
     public void setMode(int mode) {
 
         // Likely not connected to physical burner board, fallback
@@ -613,7 +473,7 @@ public class BBService extends Service {
     private long lastPressed = SystemClock.elapsedRealtime();
     private int pressCnt = 1;
 
-    private void onBatteryButton() {
+    public void onBatteryButton() {
         if (burnerBoard != null) {
             burnerBoard.showBattery();
             if ((SystemClock.elapsedRealtime() - lastPressed) < 600) {
---
+++ bb/src/main/java/com/richardmcdougall/bb/ButtonReceiver.java
@@ -0,0 +1,162 @@
+package com.richardmcdougall.bb;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.KeyEvent;
+
+public class ButtonReceiver extends BroadcastReceiver {
+
+    private BBService service;
+    private String TAG = "ButtonReceiver";
+
+    ButtonReceiver(BBService service){
+        this.service = service;
+    }
+
+
+    public void l(String s) {
+        Log.v(TAG, s);
+    }
+
+    public void onReceive(Context context, Intent intent) {
+        final String TAG = "mButtonReceiver";
+
+        //Log.d(TAG, "onReceive entered");
+        String action = intent.getAction();
+
+        if (ACTION.BUTTONS.equals(action)) {
+            Log.d(TAG, "Received BlueTooth key press");
+            BBService.buttons actionType = (BBService.buttons) intent.getSerializableExtra("buttonType");
+            switch (actionType) {
+                case BUTTON_KEYCODE:
+                    l("BUTTON_KEYCODE ");
+                    int keyCode = intent.getIntExtra("keyCode", 0);
+                    KeyEvent event = (KeyEvent) intent.getParcelableExtra("keyEvent");
+                    onKeyDown(keyCode, event);
+                    break;
+                case BUTTON_TRACK:
+                    l("BUTTON_TRACK");
+                    service.musicPlayer.NextStream();
+                    break;
+                case BUTTON_MODE_UP:
+                    l("BUTTON_MODE_UP");
+                    service.setMode(99);
+                    break;
+                case BUTTON_MODE_DOWN:
+                    l("BUTTON_MODE_DOWN");
+                    service.setMode(98);
+                    break;
+                case BUTTON_DRIFT_DOWN:
+                    l("BUTTON_DRIFT_DOWN");
+                    service.musicPlayer.MusicOffset(-10);
+                    break;
+                case BUTTON_DRIFT_UP:
+                    l("BUTTON_DRIFT_UP");
+                    service.musicPlayer.MusicOffset(10);
+                    break;
+                case BUTTON_VOL_DOWN:
+                    l("BUTTON_VOL_DOWN");
+                    service.musicPlayer.onVolDown();
+                    break;
+                case BUTTON_VOL_UP:
+                    l("BUTTON_VOL_UP");
+                    service.musicPlayer.onVolUp();
+                    break;
+                case BUTTON_VOL_PAUSE:
+                    l("BUTTON_VOL_PAUSE");
+                    service.musicPlayer.onVolPause();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (!BurnerBoardUtil.kIsRPI) {
+            return onKeyDownBurnerBoard(keyCode, event);
+        } else {
+            return onKeyDownRPI(keyCode, event);
+        }
+    }
+
+    public boolean onKeyDownBurnerBoard(int keyCode, KeyEvent event) {
+        boolean handled = false;
+        if (event.getRepeatCount() == 0) {
+            l("BurnerBoard Keycode:" + keyCode);
+        }
+
+        switch (keyCode) {
+            case 100:
+            case 87: // satachi right button
+                service.musicPlayer.NextStream();
+                break;
+            case 97:
+            case 20:
+                service.musicPlayer.MusicOffset(-10);
+                break;
+            case 19:
+                service.musicPlayer.MusicOffset(10);
+                break;
+            case 24:   // native volume up button
+            case 21:
+                service.musicPlayer.onVolUp();
+
+                return false;
+            case 25:  // native volume down button
+            case 22:
+                service.musicPlayer.onVolDown();
+                return false;
+            case 85: // Play button - show battery
+                service.onBatteryButton();
+                break;
+            case 99:
+                service.setMode(99);
+                break;
+            case 98:
+                service.setMode(98);
+                break;
+            case 88: //satachi left button
+                service.setMode(99);
+                break;
+        }
+        return true;
+    }
+
+    public boolean onKeyDownRPI(int keyCode, KeyEvent event) {
+        boolean handled = false;
+        if (event.getRepeatCount() == 0) {
+            l("RPI Keycode:" + keyCode);
+        }
+
+        switch (keyCode) {
+            case 85: // satachi Play button
+                service.boardVisualization.NextVideo();
+                break;
+
+            /* Audio stream control */
+            case 87: // satachi right button
+                l("RPI Bluetooth Right Button");
+                service.musicPlayer.NextStream();
+                break;
+            case 88: //satachi left button
+                l("RPI Bluetooth Left Button");
+                service.musicPlayer.PreviousStream();
+                break;
+
+            /* Volume control */
+            case 24:   // satachi & native volume up button
+                l("RPI Bluetooth Volume Up Button");
+                service.musicPlayer.onVolUp();
+                return false;
+            case 25:  // satachi & native volume down button
+                l("RPI Bluetooth Volume Down Button");
+                service. musicPlayer.onVolDown();
+                return false;
+        }
+        return true;
+    }
+}
