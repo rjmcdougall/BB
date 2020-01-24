e8977e6c1f360b9300d8f1c7a264da0fbfbf739d
Dan Wilson <danielkeithw@gmail.com>
2020-01-23 19:14:31 -0800

    drastically simplify musicplayersupervisor

--- bb/src/main/java/com/richardmcdougall/bb/MusicPlayerSupervisor.java
+++ bb/src/main/java/com/richardmcdougall/bb/MusicPlayerSupervisor.java
@@ -3,7 +3,6 @@ package com.richardmcdougall.bb;
 public class MusicPlayerSupervisor {
     private String TAG = this.getClass().getSimpleName();
 
-    private int musicState = 0;
     private BBService service;
 
     MusicPlayerSupervisor(BBService service) {
@@ -11,54 +10,26 @@ public class MusicPlayerSupervisor {
     }
 
     public void Run() {
-        Thread t = new Thread(new Runnable() {
-            public void run() {
-                Thread.currentThread().setName("BB Music Player");
-                BLog.i(TAG, "Starting Music Supervisor");
-                SupervisorThread();
-            }
+
+        service.musicPlayer.RadioMode();
+
+        Thread t = new Thread(() -> {
+            Thread.currentThread().setName("BB Music Player");
+            BLog.i(TAG, "Starting Music Supervisor");
+            SupervisorThread();
         });
         t.start();
     }
 
     void SupervisorThread() {
         while (true) {
-            switch (musicState) {
-                case 0:
-                    if (service.mediaManager.GetTotalAudio() != 0) {
-                        musicState = 1;
 
-                        service.musicPlayer.RadioMode();
+            service.musicPlayer.SeekAndPlay();
 
-                    } else {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (Throwable e) {
-                        }
-                    }
-                    break;
-
-                case 1:
-                    service.musicPlayer.SeekAndPlay();
-
-                    try {
-                        // RMC try 15 seconds instead of 1
-                        Thread.sleep(1000);
-                    } catch (Throwable e) {
-                    }
-                    if (service.boardState.currentRadioChannel == 0) {
-                        musicState = 2;
-                    }
-                    break;
-
-                case 2:
-                    if (service.boardState.currentRadioChannel != 0) {
-                        musicState = 1;
-                    }
-                    break;
-
-                default:
-                    break;
+            try {
+                // RMC try 15 seconds instead of 1
+                Thread.sleep(1000);
+            } catch (Throwable e) {
             }
         }
     }
