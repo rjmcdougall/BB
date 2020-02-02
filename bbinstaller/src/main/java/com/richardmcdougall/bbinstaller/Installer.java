704e7bc8f4fd3443aac667ff6f120dc626e0d904
Dan Wilson <danielkeithw@gmail.com>
2020-02-02 15:40:36 -0800

    remove dead code and duplicate code, switch threading mechanics for simplicty.

--- bbinstaller/src/main/java/com/richardmcdougall/bbinstaller/Installer.java
+++ bbinstaller/src/main/java/com/richardmcdougall/bbinstaller/Installer.java
@@ -1,63 +1,47 @@
 package com.richardmcdougall.bbinstaller;
 
 import android.app.Service;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.speech.tts.TextToSpeech;
-import android.util.Log;
 
 import com.richardmcdougall.bbcommon.AllBoards;
 import com.richardmcdougall.bbcommon.BBWifi;
 import com.richardmcdougall.bbcommon.BLog;
 import com.richardmcdougall.bbcommon.BoardState;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.richardmcdougall.bbcommon.FileHelpers;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.math.BigInteger;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.nio.ByteOrder;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Installer extends Service {
 
     public BBDownloadManager dlManager;
     int mVersion = 0;
     TextToSpeech voice;
-    private Context mContext;
-    WifiManager mWiFiManager = null;
     List<ApplicationInfo> packages = null;
-    String TAG = "BBI.Installer";
+    private String TAG = this.getClass().getSimpleName();
     int mUpgradeToVersion = 0;
     boolean mDoReboot = false;
-    private ConnectivityManager mConnMan;
-    private static boolean hasWifi = false;
-    private static boolean hasMmobile = false;
-    IntentFilter intentf = new IntentFilter();
+    ScheduledThreadPoolExecutor sch = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(1);
+    private Context context = null;
+    private AllBoards allBoards = null;
+    private BoardState boardState = null;
+    private BBWifi wifi = null;
 
     public Installer() {
-    }
-
-    public void l(String s) {
-        Log.v(TAG, s);
     }
 
     @Override
@@ -66,97 +50,15 @@ public class Installer extends Service {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    public static String getBoardId() {
-
-        String id;
-        String serial = Build.SERIAL;
-
-        if (Build.MODEL.contains("rpi3")) {
-            id = "pi" + serial.substring(Math.max(serial.length() - 6, 0),
-                    serial.length());
-        } else {
-            id = Build.MODEL;
-        }
-
-        return id;
-    }
-
-    private boolean checkWifiSSid(String ssid) {
-
-        String aWifi = "\"" + ssid + "\"";
-
-        try {
-            List<WifiConfiguration> wifiList = mWiFiManager.getConfiguredNetworks();
-            if (wifiList != null) {
-                for (WifiConfiguration config : wifiList) {
-                    String newSSID = config.SSID;
-                    l("Found wifi:" + newSSID + " == " + aWifi + " ?");
-                    if (aWifi.equals(newSSID)) {
-                        return true;
-                    }
-                }
-            }
-        } catch (Exception e) {
-        }
-        return false;
-    }
-
-
-    private void connectWifi(String ssid) {
-
-        String aWifi = "\"" + ssid + "\"";
-
-        try {
-            List<WifiConfiguration> wifiList = mWiFiManager.getConfiguredNetworks();
-            if (wifiList != null) {
-                for (WifiConfiguration config : wifiList) {
-                    String newSSID = config.SSID;
-
-                    l("Found wifi:" + newSSID + " == " + aWifi + " ?");
-
-                    if (aWifi.equals(newSSID)) {
-                        l("connecting wifi:" + newSSID);
-                        mWiFiManager.disconnect();
-                        mWiFiManager.enableNetwork(config.networkId, true);
-                        mWiFiManager.reconnect();
-
-                        return;
-                    }
-                }
-            }
-        } catch (Exception e) {
-        }
-    }
-
-    private void addWifi(String ssid, String pass) {
-        try {
-
-            WifiConfiguration conf = new WifiConfiguration();
-            conf.SSID = "\"" + ssid + "\"";
-            conf.preSharedKey = "\"" + pass + "\"";
-            mWiFiManager.addNetwork(conf);
-            mWiFiManager.disconnect();
-            mWiFiManager.enableNetwork(conf.networkId, false);
-            mWiFiManager.reconnect();
-        } catch (Exception e) {
-        }
-        return;
-    }
-
-    private Context context = null;
-    private AllBoards allBoards = null;
-    private BoardState boardState = null;
-    private BBWifi wifi = null;
 
     @Override
     public void onCreate() {
 
         super.onCreate();
 
-        l("Starting BB Installer");
+        BLog.i(TAG,"Starting BB Installer");
 
         BLog.i(TAG, "onCreate");
-
 
         voice = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -177,13 +79,12 @@ public class Installer extends Service {
 
         allBoards = new AllBoards(context, voice);
 
-        try{
+        try {
             while (allBoards.dataBoards == null) {
                 BLog.i(TAG, "Boards file is required to be downloaded before proceeding.  Please hold.");
                 Thread.sleep(2000);
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             BLog.e(TAG, e.getMessage());
         }
 
@@ -199,12 +100,12 @@ public class Installer extends Service {
         BLog.i(TAG, "State Tyoe " + boardState.boardType);
         BLog.i(TAG, "Display Teensy " + boardState.displayTeensy);
 
-        wifi = new BBWifi(context,boardState);
+        wifi = new BBWifi(context, boardState);
 
         getPackageVersions();
 
         dlManager = new BBDownloadManager(getApplicationContext().getFilesDir().getAbsolutePath(), mVersion);
-        dlManager.onProgressCallback = new BBDownloadManager.OnDownloadProgressType() {
+        dlManager.onProgressCallback = new FileHelpers.OnDownloadProgressType(){
             long lastTextTime = 0;
 
             public void onProgress(String file, long fileSize, long bytesDownloaded) {
@@ -220,7 +121,7 @@ public class Installer extends Service {
                         voice.speak("Downloading New Software " + file + ", " + String.valueOf(percent) + " Percent", TextToSpeech.QUEUE_ADD, null, "downloading");
                     }
                     lastTextTime = curTime;
-                    l(String.format("Downloading %02x%% %s", bytesDownloaded * 100 / fileSize, file));
+                    BLog.d(TAG,String.format("Downloading %02x%% %s", bytesDownloaded * 100 / fileSize, file));
                 }
             }
 
@@ -230,65 +131,50 @@ public class Installer extends Service {
 
         };
 
-        dlManager.StartDownloads();
+        Runnable checkForInstall = () -> installerThread();
 
-        Thread installerThread = new Thread(new Runnable() {
-            public void run() {
-                Thread.currentThread().setName("BBInstaller Main");
-                installerThread();
-            }
-        });
-
-        installerThread.start();
+        sch.scheduleWithFixedDelay(checkForInstall, 5, 60, TimeUnit.SECONDS);
 
     }
 
 
     public void installerThread() {
 
-        while (true) {
-            try {
+        try {
 
-                int currentBBVersion = getBBversion();
+            int currentBBVersion = getBBversion();
 
-                if(boardState.targetAPKVersion > 0 && boardState.targetAPKVersion != currentBBVersion) {
-                    String apkFile = null;
-                    int thisBFileId = 0;
-                    try {
-                        thisBFileId = dlManager.GetAPKByVersion("bb", boardState.targetAPKVersion);
-                        apkFile = dlManager.GetAPKFile(thisBFileId);
-                    } catch (Exception e) {
-                        l("Version not yet downloaded");
-                    }
-
-                    if (apkFile != null) {
-                        if (installApk(apkFile)) {
-                            l("Installed BB version " + mUpgradeToVersion);
-                            voice.speak("Installed Software version " + mUpgradeToVersion, TextToSpeech.QUEUE_ADD, null, "swvers");
-                            Thread.sleep(5000);
-                            // Should we reboot?
-                            if (mDoReboot) {
-                                voice.speak("Re booting", TextToSpeech.QUEUE_ADD, null, "swvers");
-                                Thread.sleep(3000);
-                                doReboot("Upgrade");
-                            }
-                        } else {
-                            l("Failed installing BB version " + mUpgradeToVersion);
-                            voice.speak("Failed upgrade of software version " + mUpgradeToVersion, TextToSpeech.QUEUE_ADD, null, "swversfail");
-
-                        }
-                    }
-
+            if (boardState.targetAPKVersion > 0 && boardState.targetAPKVersion != currentBBVersion) {
+                String apkFile = null;
+                int thisBFileId = 0;
+                try {
+                    thisBFileId = dlManager.GetAPKByVersion("bb", boardState.targetAPKVersion);
+                    apkFile = dlManager.GetAPKFile(thisBFileId);
+                } catch (Exception e) {
+                    BLog.e(TAG,"Version not yet downloaded");
                 }
 
-            } catch (Exception e) {
-                l("Unknown exception: " + e.getMessage());
+                if (apkFile != null) {
+                    if (installApk(apkFile)) {
+                        BLog.d(TAG,"Installed BB version " + mUpgradeToVersion);
+                        voice.speak("Installed Software version " + mUpgradeToVersion, TextToSpeech.QUEUE_ADD, null, "swvers");
+                        Thread.sleep(5000);
+                        // Should we reboot?
+                        if (mDoReboot) {
+                            voice.speak("Re booting", TextToSpeech.QUEUE_ADD, null, "swvers");
+                            Thread.sleep(3000);
+                            doReboot("Upgrade");
+                        }
+                    } else {
+                        BLog.e(TAG,"Failed installing BB version " + mUpgradeToVersion);
+                        voice.speak("Failed upgrade of software version " + mUpgradeToVersion, TextToSpeech.QUEUE_ADD, null, "swversfail");
+
+                    }
+                }
             }
 
-            try {
-                Thread.sleep(60000);
-            } catch (Exception e) {
-            }
+        } catch (Exception e) {
+            BLog.e(TAG,"Unknown exception: " + e.getMessage());
         }
     }
 
@@ -297,9 +183,8 @@ public class Installer extends Service {
         pm.reboot(reason);
     }
 
-    public boolean installApk(String path)
-    {
-        Log.i(TAG, "Installing software update " + path);
+    public boolean installApk(String path) {
+        BLog.i(TAG, "Installing software update " + path);
 
         final String libs = "LD_LIBRARY_PATH=/vendor/lib64:/system/lib64 ";
 
@@ -308,7 +193,7 @@ public class Installer extends Service {
             final String[] commands = {
                     "pm install -i com.richardmcdougall.bb --user 0 " + path
             };
-            execute_as_root(commands);
+            return execute_as_root(commands);
         } else {
             final String[] commands = {
 //                libs + "cp  " + path + " /data/local/tmp 2>&1 >>/data/local/tmp/bb.log" ,
@@ -317,23 +202,20 @@ public class Installer extends Service {
                     libs + "chmod 666 " + path,
                     libs + "pm install -rg " + path
             };
-            
-            execute_as_root(commands);
+
+            return execute_as_root(commands);
         }
-
-        return true;
-
     }
 
-    private boolean execute_as_root( String[] commands ) {
+    private boolean execute_as_root(String[] commands) {
         try {
             // Do the magic
-            Process p = Runtime.getRuntime().exec( "sh" );
+            Process p = Runtime.getRuntime().exec("sh");
             InputStream es = p.getErrorStream();
             DataOutputStream os = new DataOutputStream(p.getOutputStream());
 
-            for( String command : commands ) {
-                Log.i(TAG,command);
+            for (String command : commands) {
+                BLog.i(TAG, command);
                 os.writeBytes(command + "\n");
             }
             os.writeBytes("exit\n");
@@ -348,13 +230,13 @@ public class Installer extends Service {
             }
 
             p.waitFor();
-            Log.e(TAG, output + " (" + p.exitValue() + ")");
+            if(output.contains("Error:"))
+                throw new Exception(output);
+
+            BLog.i(TAG, output + " (" + p.exitValue() + ")");
             return true;
-        } catch (IOException e) {
-            Log.e(TAG, e.getMessage());
-            return false;
-        } catch (InterruptedException e) {
-            Log.e(TAG, e.getMessage());
+        } catch (Exception e) {
+            BLog.e(TAG, e.getMessage());
             return false;
         }
     }
@@ -365,7 +247,7 @@ public class Installer extends Service {
         try {
             PackageInfo info = pm.getPackageInfo("com.richardmcdougall.bb", 0);
             versionCode = info.versionCode;
-            l("Version: " + versionCode);
+            BLog.d(TAG,"Version: " + versionCode);
         } catch (Exception e) {
         }
         return versionCode;
--- bbinstaller/src/main/java/com/richardmcdougall/bbinstaller/onBoot.java
+++ bbinstaller/src/main/java/com/richardmcdougall/bbinstaller/onBoot.java
@@ -3,7 +3,11 @@ package com.richardmcdougall.bbinstaller;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import com.richardmcdougall.bbcommon.BLog;
 
 /**
  * Created by rmc on 10/22/17.
@@ -14,6 +18,20 @@ public class onBoot extends BroadcastReceiver {
     {
         Intent intent = new Intent(context,Installer.class);
         context.startService(intent);
-        Log.i("BBInstaller", "started");
+        BLog.i("BBInstaller", "started");
     }
 }
+
+
+//public class onBoot extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        Intent intent = new Intent(getApplicationContext(),Installer.class);
+//        getApplicationContext().startService(intent);
+//        BLog.i("BBInstaller", "started");
+//
+//    }
+//}
