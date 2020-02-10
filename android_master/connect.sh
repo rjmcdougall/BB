ed79d5d94d54fe60ec8ed596b5fd16e9e8d1ac88
Dan Wilson <danielkeithw@gmail.com>
2020-02-09 21:40:00 -0800

    remove unused files

--- android_master/connect.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "IP Address?"
-read ip
-
-~/Library/Android/sdk/platform-tools/adb connect ${ip}
-
--- android_master/install_android_only.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-~/Library/Android/sdk/platform-tools/adb install -r -g bb.apk
-
