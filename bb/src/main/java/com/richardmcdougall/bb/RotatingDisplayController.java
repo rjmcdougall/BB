db28558a3c54f22c7a815f122b625a6dc509df33
Dan Wilson <danielkeithw@gmail.com>
2020-05-08 18:26:22 -0700

    add configured durations when sign is rotating.

--- bb/src/main/java/com/richardmcdougall/bb/RotatingDisplayController.java
+++ bb/src/main/java/com/richardmcdougall/bb/RotatingDisplayController.java
@@ -10,30 +10,58 @@ public class RotatingDisplayController {
 
     private String TAG = this.getClass().getSimpleName();
     private BBService service = null;
+    private boolean isSwitching = false;
 
     ScheduledThreadPoolExecutor sch = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(1);
 
     Runnable checkForRotatingDisplay = () -> {
-        try{
-            if(service.boardState.rotatingDisplay==true)
-                SwitchDisplayMode();
-        }catch(Exception e){
+        try {
+            if (service.boardState.rotatingDisplay == true) {
+                if(!isSwitching)
+                    SwitchDisplayMode();
+                isSwitching = true;
+            }
+            else
+                isSwitching = false;
+
+        } catch (Exception e) {
             BLog.e(TAG, e.getMessage());
         }
     };
 
     public RotatingDisplayController(BBService service) {
         this.service = service;
-        sch.scheduleAtFixedRate(checkForRotatingDisplay, 10, 10, TimeUnit.SECONDS);
-   }
+        sch.scheduleAtFixedRate(checkForRotatingDisplay, 10, 1, TimeUnit.SECONDS);
+    }
 
-   public void EnableRotatingDisplay(boolean isRotatingDisplay){
+    public void EnableRotatingDisplay(boolean isRotatingDisplay) {
         this.service.boardState.rotatingDisplay = isRotatingDisplay;
-   }
+    }
+
+    Runnable switchVideo = () -> {
+        try {
+            if (service.boardState.rotatingDisplay == true)
+                SwitchDisplayMode();
+        } catch (Exception e) {
+            BLog.e(TAG, e.getMessage());
+        }
+    };
 
     private void SwitchDisplayMode() {
+
         this.service.boardVisualization.setMode(99);
         BLog.d(TAG, "Display Mode video switch to mode " + this.service.boardState.currentVideoMode);
+        int secondsBeforeSwitch = 10;
+
+        try {
+            if (this.service.mediaManager.GetVideo().has("Length")) {
+                secondsBeforeSwitch = this.service.mediaManager.GetVideo().getInt("Length");
+            }
+        } catch (Exception e) {
+            BLog.e(TAG, "Error Switching Vide Mode for Sign " + e.getMessage());
+        }
+        sch.schedule(switchVideo, secondsBeforeSwitch, TimeUnit.SECONDS);
+
     }
 
 }
