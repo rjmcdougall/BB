f8dab849a564eb344107356caf9f57880f6dbbeb
Daniel Wilson <danielkeithw@gmail.com>
2023-05-20 11:54:20 -0700

    eliminate cloud as a display map source. instead we will now always check every 1 second for a new file.

--- bb/src/main/java/com/richardmcdougall/bb/DisplayMapManager.java
+++ bb/src/main/java/com/richardmcdougall/bb/DisplayMapManager.java
@@ -6,7 +6,6 @@ import java.util.Random;
 import com.richardmcdougall.bb.board.TranslationMap;
 import com.richardmcdougall.bbcommon.BLog;
 import com.richardmcdougall.bbcommon.FileHelpers;
-import com.richardmcdougall.bb.board.TranslationMap;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -21,179 +20,60 @@ import java.util.Hashtable;
 public class DisplayMapManager {
 
     private String TAG = this.getClass().getSimpleName();
-
     private String displauMapJSONFilename;
-    private static final String DISPLAY_MAP_TMP_FILENAME = "displayMap.json.tmp";
-    private static final String DISPLAY_MAP_TEMP2_FILENAME = "displayMapTemp";
-    private static final String DIRECTORY_URL = "https://storage.googleapis.com/burner-board/BurnerBoardDisplayTranslation/";
-
     private BBService service;
     public JSONArray dataDisplayMap;
     ScheduledThreadPoolExecutor sch = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(1);
     private String filesDir;
-    boolean downloadSuccessDisplayMap = false;
-    private int  maxFailedChecks = 60;
-    private int currentFailedChecks = 0;
     public FileHelpers.OnDownloadProgressType onProgressCallback = null;
     public ArrayList<TranslationMap> displayMap;
     public int boardHeight = 0;
     public int boardWidth = 0;
     public int numberOfStrips = 0;
-    public boolean debug = false;
-    private Random rand = new Random();
-    String origDir = "";
+    String displayMapText = "";
+    long lastDisplayMapModified = 0;
+
 
     public DisplayMapManager(BBService service) {
         this.service = service;
         filesDir = this.service.context.getFilesDir().getAbsolutePath();
 
         this.displauMapJSONFilename = this.service.boardState.BOARD_ID + ".json";
-        LoadInitialDisplayMap();
 
-        // wait 5 seconds to hopefully get wifi before starting the download.
-        Runnable initialCheckForDisplayMap = () -> initialCheck();
-        Runnable periodicCheckForDisplayMap = () -> periodicCheck();
-        Runnable debugCheckForDisplayMap = () -> frequentCheck();
-        Runnable checkForDebugStatus = () -> checkForDebug();
+        Runnable periodicCheckForDisplayMap = () -> LoadDisplayMap();
 
-        sch.scheduleWithFixedDelay(initialCheckForDisplayMap, 1, 5, TimeUnit.SECONDS);
-        sch.scheduleWithFixedDelay(periodicCheckForDisplayMap, 30, 30, TimeUnit.SECONDS);
-        sch.scheduleWithFixedDelay(debugCheckForDisplayMap, 5, 10, TimeUnit.SECONDS);
-        sch.scheduleWithFixedDelay(checkForDebugStatus, 1, 1, TimeUnit.SECONDS);
+        sch.scheduleWithFixedDelay(periodicCheckForDisplayMap, 1, 1, TimeUnit.SECONDS);
 
-        this.onProgressCallback = new FileHelpers.OnDownloadProgressType() {
-            long lastTextTime = 0;
-
-            public void onProgress(String file, long fileSize, long bytesDownloaded) {
-                if (fileSize <= 0)
-                    return;
-
-                long curTime = System.currentTimeMillis();
-                if (curTime - lastTextTime > 30000) {
-                    lastTextTime = curTime;
-                    long percent = bytesDownloaded * 100 / fileSize;
-
-                  //  service.speak("Downloading " + file + "", "downloading display");
-                    lastTextTime = curTime;
-                    BLog.d(TAG, "Downloading " + file + ", " + percent + " Percent");
-                }
-            }
-
-            public void onVoiceCue(String msg) {
-                //voice.speak(msg, TextToSpeech.QUEUE_ADD, null,"Download Message");
-            }
-        };
     }
 
-    private void initialCheck(){
-        currentFailedChecks++;
-
-        if (!downloadSuccessDisplayMap && currentFailedChecks < maxFailedChecks)
-            downloadSuccessDisplayMap = GetNewDisplayMapJSON();
-    }
-
-    private void checkForDebug(){
-        boolean currentDebug = this.debug;
-        boolean newDebug = this.service.boardState.displayDebug;
-
-        if(!currentDebug && newDebug)
-            this.service.speak("Display Debug On", "debug status");
-
-        if(currentDebug && !newDebug)
-            this.service.speak("Display Debug Off", "debug status");
-
-        this.debug = newDebug;
-    }
-
-    private void periodicCheck(){
-        if(!debug)
-            downloadSuccessDisplayMap = GetNewDisplayMapJSON();
-    }
-    private void frequentCheck(){
-        if(debug)
-
-            LoadInitialDisplayMap();
-    }
-
-    private void LoadInitialDisplayMap() {
+    private boolean LoadDisplayMap() {
         try {
-            File[] flist = new File(filesDir).listFiles();
-            if (flist != null) {
 
-                origDir = FileHelpers.LoadTextFile(this.displauMapJSONFilename, filesDir);
-                if (origDir != null) {
-                    JSONArray dir = new JSONArray(origDir);
+            File file = new File(filesDir, this.displauMapJSONFilename);
+
+            if (file.exists()) {
+                long lastModified = file.lastModified();
+
+                if (lastModified > lastDisplayMapModified) {
+
+                    displayMapText = FileHelpers.LoadTextFile(this.displauMapJSONFilename, filesDir);
+
+                    JSONArray dir = new JSONArray(displayMapText);
 
                     dataDisplayMap = dir;
                     CreateDisplayMapFromJSON();
                     service.burnerBoard.initpixelMap2Board();
 
-                    BLog.d(TAG, "Dir " + origDir.substring(0,100));
+                    BLog.d(TAG, "new display map loaded " + displayMapText.substring(0,100));
+                    lastDisplayMapModified = lastModified;
                 }
             }
 
         } catch (Throwable er) {
-            BLog.e(TAG, er.getMessage());
-        }
-    }
-
-    String dirTxt;
-
-    private boolean GetNewDisplayMapJSON() {
-
-        try {
-
-            String cacheBuster = "?" + String.valueOf(rand.nextInt());
-
-            boolean returnValue = true;
-
-            BLog.d(TAG, DIRECTORY_URL);
-
-            long ddsz = -1;
-            ddsz = FileHelpers.DownloadURL(DIRECTORY_URL + this.displauMapJSONFilename + cacheBuster, DISPLAY_MAP_TEMP2_FILENAME, "Display Map JSON", onProgressCallback, this.filesDir);
-
-            if (ddsz < 0) {
-                BLog.d(TAG, "Unable to Download Display Map JSON.  Likely because of no internet.  Sleeping for 5 seconds. ");
-                returnValue = false;
-            } else {
-                new File(filesDir, DISPLAY_MAP_TEMP2_FILENAME).renameTo(new File(filesDir, DISPLAY_MAP_TMP_FILENAME));
-
-                dirTxt = FileHelpers.LoadTextFile(DISPLAY_MAP_TMP_FILENAME, filesDir);
-                JSONArray dir = new JSONArray(dirTxt);
-
-                BLog.d(TAG, "Downloaded Display Map JSON: " + dirTxt.substring(0,10));
-
-                if (onProgressCallback != null) {
-                    if (!dirTxt.equalsIgnoreCase(origDir)) {
-                        BLog.d(TAG, "New Display Map Synced.");
-
-                        // got new display map.  Update!
-                        dataDisplayMap = dir;
-                        CreateDisplayMapFromJSON();
-                        service.burnerBoard.initpixelMap2Board();
-
-                        // Update the directory so the board can use it.
-                        new File(filesDir, DISPLAY_MAP_TMP_FILENAME).renameTo(new File(filesDir, this.displauMapJSONFilename));
-                        origDir = FileHelpers.LoadTextFile(this.displauMapJSONFilename, filesDir);
-
-                        onProgressCallback.onVoiceCue("New Display Map Synced.");
-                    } else
-                        BLog.d(TAG, "no changes discovered in Display Map JSON.");
-                }
-
-                if (dataDisplayMap != null)
-                    if (dir.toString().length() == dataDisplayMap.toString().length()) {
-                        BLog.d(TAG, "No Changes to Display Map JSON.");
-                        returnValue = true;
-                    }
-            }
-
-
-            return returnValue;
-        } catch (Exception e) {
-            BLog.e(TAG, e.getMessage());
+            BLog.e(TAG, "Failed to load display map. This is normal during startup.  " + er.getMessage());
             return false;
         }
+        return true;
     }
 
     public TranslationMap[] GetDisplayMap(){
@@ -201,7 +81,6 @@ public class DisplayMapManager {
         a = displayMap.toArray(a);
         return a;
     }
-
 
     Hashtable<Integer, Integer> offsets = new Hashtable<Integer, Integer> ();
     JSONObject displayMapTemp;
@@ -232,7 +111,6 @@ public class DisplayMapManager {
         }
 
     }
-
 
     private void CreateDisplayMapFromJSON(){
 
