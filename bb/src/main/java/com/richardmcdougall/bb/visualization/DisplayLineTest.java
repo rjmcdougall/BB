bab25c167e909ea938d897775b85ef0b4903fd23
Daniel Wilson <danielkeithw@gmail.com>
2023-07-08 17:50:56 -0700

    fix linetest and rowtest to work without displaymap2

--- bb/src/main/java/com/richardmcdougall/bb/visualization/DisplayLineTest.java
+++ bb/src/main/java/com/richardmcdougall/bb/visualization/DisplayLineTest.java
@@ -16,15 +16,27 @@ public class DisplayLineTest extends Visualization {
 
     public void update(int mode) {
 
-        boolean isEven = this.service.displayMapManager2.boardWidth % 2 == 0 ? true : false;
+        boolean isEven = this.service.displayMapManager.boardWidth % 2 == 0 ? true : false;
 
-        for(int y = 0; y<this.service.displayMapManager2.boardHeight;y++ ){
-            if(!isEven) {
-                service.burnerBoard.setPixel((this.service.displayMapManager2.boardWidth + 1) / 2,y,0,255,0);
-                service.burnerBoard.setPixel((this.service.displayMapManager2.boardWidth - 1) / 2,y,0,255,0);
-            }
-            else {
-                service.burnerBoard.setPixel((this.service.displayMapManager2.boardWidth) / 2,y,0,255,0);
+        for(int y = 0; y<this.service.displayMapManager.boardHeight;y++ ){
+
+            switch(y % 10) {
+                case 0:
+                    if (!isEven) {
+                        service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth + 1) / 2, y, 0, 255, 0);
+                        service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth - 1) / 2, y, 0, 255, 0);
+                    } else {
+                        service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth) / 2, y, 0, 255, 0);
+                    }
+                    break;
+                default:
+                    if (!isEven) {
+                        service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth + 1) / 2, y, 255, 255, 255);
+                        service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth - 1) / 2, y, 255, 255, 255);
+                    } else {
+                        service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth) / 2, y, 255, 255, 255);
+                    }
+                    break;
             }
         }
 
--- bb/src/main/java/com/richardmcdougall/bb/visualization/DisplayRowTest.java
+++ bb/src/main/java/com/richardmcdougall/bb/visualization/DisplayRowTest.java
@@ -16,25 +16,37 @@ public class DisplayRowTest extends Visualization {
 
     public void update(int mode) {
 
-        for (DisplayMapManager2.DisplayMapRow row : this.service.displayMapManager2.displayMap) {
-            for(int x = 0; x < row.rowLength(); x++){
-                switch(row.row % 5) {
+        for(int y=0; y < this.service.burnerBoard.boardHeight;y++){
+             for(int x = 0; x < this.service.burnerBoard.boardWidth; x++){
+                switch(y % 5) {
                     case 0:
-                        service.burnerBoard.setPixel(x,row.row,255,0,0);
+                        service.burnerBoard.setPixel(x,y,255,0,0);
                         break;
                     case 1:
-                        service.burnerBoard.setPixel(x,row.row,255,255,255);
+                        service.burnerBoard.setPixel(x,y,0,255,255);
                         break;
                     case 2:
-                        service.burnerBoard.setPixel(x,row.row,0,0,255);
+                        service.burnerBoard.setPixel(x,y,0,0,255);
                         break;
                     case 3:
-                        service.burnerBoard.setPixel(x,row.row,0,255,0);
+                        service.burnerBoard.setPixel(x,y,0,255,0);
                         break;
                     case 4:
-                        service.burnerBoard.setPixel(x,row.row,255,255,0);
+                        service.burnerBoard.setPixel(x,y,255,255,0);
                         break;
                 }
+            }
+        }
+
+        boolean isEven = this.service.displayMapManager.boardWidth % 2 == 0 ? true : false;
+
+        for(int y = 0; y<this.service.displayMapManager.boardHeight;y++ ){
+            if(!isEven) {
+                service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth + 1) / 2,y,255,255,255);
+                service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth - 1) / 2,y,255,255,255);
+            }
+            else {
+                service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth) / 2,y,255,255,255);
             }
         }
         service.burnerBoard.flush();
