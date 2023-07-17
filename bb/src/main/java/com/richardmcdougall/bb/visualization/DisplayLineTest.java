25bff52e09ec59281c6d46d17f1e3bd501d9dac1
Daniel Wilson <danielkeithw@gmail.com>
2023-07-16 22:45:06 -0700

    change DisplayLineTest to place a green pixel every 10 rows from the top instead of the bottom because v4 is reversed.

--- bb/src/main/java/com/richardmcdougall/bb/visualization/DisplayLineTest.java
+++ bb/src/main/java/com/richardmcdougall/bb/visualization/DisplayLineTest.java
@@ -10,35 +10,31 @@ public class DisplayLineTest extends Visualization {
 
     public DisplayLineTest(BBService service) {
         super(service);
-
     }
-
-
     public void update(int mode) {
 
         boolean isEven = this.service.displayMapManager.boardWidth % 2 == 0 ? true : false;
 
-        for(int y = 0; y<this.service.displayMapManager.boardHeight;y++ ){
+        service.burnerBoard.clearPixels();
 
-            switch(y % 10) {
-                case 0:
-                    if (!isEven) {
-                        service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth + 1) / 2, y, 0, 255, 0);
-                        service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth - 1) / 2, y, 0, 255, 0);
-                    } else {
-                        service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth) / 2, y, 0, 255, 0);
-                    }
-                    break;
-                default:
-                    if (!isEven) {
-                        service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth + 1) / 2, y, 255, 255, 255);
-                        service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth - 1) / 2, y, 255, 255, 255);
-                    } else {
-                        service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth) / 2, y, 255, 255, 255);
-                    }
-                    break;
+        for(int y = 0; y<this.service.displayMapManager.boardHeight;y++ ){
+            if (!isEven) {
+                service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth + 1) / 2, y, 255, 255, 255);
+                service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth - 1) / 2, y, 255, 255, 255);
+            } else {
+                service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth) / 2, y, 255, 255, 255);
             }
         }
+        int y;
+        for(y = this.service.displayMapManager.boardHeight - 1; y>=0; y = y - 10 ) {
+            if (!isEven) {
+                service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth + 1) / 2, y, 0, 255, 0);
+                service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth - 1) / 2, y, 0, 255, 0);
+            } else {
+                service.burnerBoard.setPixel((this.service.displayMapManager.boardWidth) / 2, y, 0, 255, 0);
+            }
+        }
+
 
         service.burnerBoard.flush();
     }
