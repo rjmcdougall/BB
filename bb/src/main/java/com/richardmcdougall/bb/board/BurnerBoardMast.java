5944763ac1c654de21fe36c749f6ac05b06d3272
rjmcdougall <github@3emus.com>
2024-10-22 21:48:01 -0700

    Reduce power for mast

--- bb/src/main/java/com/richardmcdougall/bb/board/BurnerBoardMast.java
+++ bb/src/main/java/com/richardmcdougall/bb/board/BurnerBoardMast.java
@@ -56,7 +56,7 @@ public class BurnerBoardMast extends BurnerBoard {
         int[] mOutputScreen = boardScreen.clone();
         mOutputScreen = this.textBuilder.renderText(mOutputScreen);
         mOutputScreen = this.lineBuilder.renderLine(mOutputScreen);
-        mOutputScreen = mDimmer.Dim(12, mOutputScreen);
+        mOutputScreen = mDimmer.Dim(2, mOutputScreen);
         this.appDisplay.send(mOutputScreen);
 
         // Walk through each strip and fill from the graphics buffer
